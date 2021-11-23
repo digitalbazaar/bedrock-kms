4cbdc9f3849a74c97f69d55668d79c4cb6a58dfc
Dave Longley <dlongley@digitalbazaar.com>
2021-11-22 19:30:33 -0500

    Add `aggregate` test.

--- test/mocha/14-keystores-getStorageUsage-api.js
+++ test/mocha/14-keystores-getStorageUsage-api.js
@@ -7,20 +7,20 @@ const {keystores, defaultModuleManager: moduleManager} = require('bedrock-kms');
 
 describe('keystores APIs', () => {
   describe('getStorageUsage API', () => {
-    it('successfully gets storage usage for a keystore', async () => {
+    it('gets storage usage for a keystore', async () => {
       let err;
       let result;
       const config = {
-        id: 'https://example.com/keystores/usage-test',
+        id: 'https://example.com/keystores/usage-test1',
         controller: 'usage-test',
         kmsModule: 'ssm-v1',
         sequence: 0,
-        meterId: 'foo'
+        meterId: 'usage-meter-1'
       };
       try {
         await keystores.insert({config});
         result = await keystores.getStorageUsage({
-          meterId: 'foo', moduleManager
+          meterId: 'usage-meter-1', moduleManager
         });
       } catch(e) {
         err = e;
@@ -29,6 +29,32 @@ describe('keystores APIs', () => {
       should.exist(result);
       result.should.be.an('object');
       result.should.deep.equal({storage: 1});
+    });
+    it('gets custom storage usage for a keystore', async () => {
+      let err;
+      let result;
+      const config = {
+        id: 'https://example.com/keystores/usage-test2',
+        controller: 'usage-test',
+        kmsModule: 'ssm-v1',
+        sequence: 0,
+        meterId: 'usage-meter-2'
+      };
+      try {
+        await keystores.insert({config});
+        result = await keystores.getStorageUsage({
+          meterId: 'usage-meter-2', moduleManager,
+          async aggregate({usage}) {
+            usage.storage++;
+          }
+        });
+      } catch(e) {
+        err = e;
+      }
+      assertNoError(err);
+      should.exist(result);
+      result.should.be.an('object');
+      result.should.deep.equal({storage: 2});
     });
   }); // end getStorageUsage API
 }); // end keystore APIs
