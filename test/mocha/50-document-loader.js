413ced5543009e3639fb935a440dd3a10767ecba
Josh Dunn <jdunn@digitalbazaar.com>
2021-12-17 14:41:53 -0700

    Add additional tests for better coverage.

---
+++ test/mocha/50-document-loader.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const brKms = require('bedrock-kms');
+const documentLoader = brKms.defaultDocumentLoader;
+
+describe('defaultDocumentLoader', () => {
+  it('returns a did document from the document loader', async () => {
+    const url = 'did:key:z6MkpTHR8VNsBxYAAWHut2Geadd9jSwuBV8xRoAnwWsdvktH';
+
+    let err;
+    let result;
+    try {
+      result = await documentLoader(url);
+    } catch(e) {
+      err = e;
+    }
+
+    should.exist(result);
+    should.not.exist(err);
+    result.should.have.keys(['contextUrl', 'documentUrl', 'document']);
+    result.documentUrl.should.equal(url);
+  });
+
+  it('throws NotFoundError on document not found', async () => {
+    const url = 'https://example.com/foo.jsonld';
+
+    let err;
+    let result;
+    try {
+      result = await documentLoader(url);
+    } catch(e) {
+      err = e;
+    }
+
+    should.not.exist(result);
+    should.exist(err);
+    err.should.be.instanceOf(Error);
+    err.message.should.contain(url);
+  });
+});
