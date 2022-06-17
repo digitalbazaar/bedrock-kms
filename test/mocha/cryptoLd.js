189d61ed0396a8a7c3f29c689e15f890eedabdb4
David I. Lehn <dlehn@digitalbazaar.com>
2022-06-17 00:43:34 -0400

    Update dependencies.

--- test/mocha/cryptoLd.js
+++ test/mocha/cryptoLd.js
@@ -1,17 +1,19 @@
 /*!
  * Copyright (c) 2021-2022 Digital Bazaar, Inc. All rights reserved.
  */
-import {createRequire} from 'node:module';
-const require = createRequire(import.meta.url);
-const {CryptoLD} = require('crypto-ld');
-const {Ed25519VerificationKey2018} =
-  require('@digitalbazaar/ed25519-verification-key-2018');
-const {Ed25519VerificationKey2020} =
-  require('@digitalbazaar/ed25519-verification-key-2020');
-const {X25519KeyAgreementKey2019} =
-  require('@digitalbazaar/x25519-key-agreement-key-2019');
-const {X25519KeyAgreementKey2020} =
-  require('@digitalbazaar/x25519-key-agreement-key-2020');
+import {CryptoLD} from 'crypto-ld';
+import {
+  Ed25519VerificationKey2018
+} from '@digitalbazaar/ed25519-verification-key-2018';
+import {
+  Ed25519VerificationKey2020
+} from '@digitalbazaar/ed25519-verification-key-2020';
+import {
+  X25519KeyAgreementKey2019
+} from '@digitalbazaar/x25519-key-agreement-key-2019';
+import {
+  X25519KeyAgreementKey2020
+} from '@digitalbazaar/x25519-key-agreement-key-2020';
 
 const cryptoLd = new CryptoLD();
 cryptoLd.use(Ed25519VerificationKey2018);
